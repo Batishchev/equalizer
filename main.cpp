5d0b2e7fb17ceaf129aea27e20cdf091a45232a5
pavel <pavel@pavel-EX610>
2012-12-11 21:40:56 +0400

    Убраны комментарии.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <cmath>
 #include <bass.h>
 #include <ncurses.h>
-#include <unistd.h> // for sleep()
+#include <unistd.h>
+//#include <unistd.h> // for sleep()
 
 using namespace std;
 
@@ -13,11 +14,8 @@ int main(int argc, char* argv[])
     char* filename;
     float fft[128];
     int current, i, j;
-    char result[1950];// = new char[1950]; //30*65
+    char result[1950];  //30*65
     int prevfft[64];
-    
-    /*chtype out[30][64];
-    const char* destPtr; */
     
     for(i=0;i<30;i++)
         for(j=0;j<65;j++){
@@ -29,7 +27,6 @@ int main(int argc, char* argv[])
     
     for(j=0;j<64;j++)
         prevfft[i] = 0;
-            
     
     if(argc != 2) {
         addstr( "Программа принимает 1 аргумент - имя проигрываемого wav файла.\n" );
@@ -42,9 +39,6 @@ int main(int argc, char* argv[])
         refresh();
         return 0;
     }
-    
-    //raw();
-    //noecho();
     
     filename = argv[1];
     
@@ -72,53 +66,30 @@ int main(int argc, char* argv[])
         BASS_ChannelGetData(stream, fft, BASS_DATA_FFT256);
         
         for(i=0;i<64;i++){
-            current = ceil(abs(fft[i]) * 100);
+            current = ceil(fabs(fft[i]) * 100);
             if(current > 29)
                 current = 29;
             
-            if(current == prevfft[i])
-                continue;
-            
-            if(current < prevfft[i]){
+            if(current <= prevfft[i]){
                 j = prevfft[i];
                 result[(29-j)*65+i] = ' ';
                 prevfft[i] = j-1;
                 continue;
             }
             
-            for(j=current;j>=prevfft[i];j--)
+            for(j=current-1;j>=prevfft[i];j--)
                 result[(29-j)*65+i] = '*';
             
             prevfft[i] = current;
         }
         
         move(0,0);
-        //clear();
         printw(result);
         refresh();
-        sleep (0.5);
+        usleep(100000);
     }
-    /* останавливаем звучание */
+    
     BASS_ChannelStop(stream);
-    /* освобождаем ресурсы */
     BASS_StreamFree(stream); 
-    
     endwin();
-            
-            
-    
-    
-    
-    /*for (;;) {
-        std::cout << "  |\n" << std::flush;
-        sleep(1);
-        std::cout << "  | |\n" << std::flush;
-        sleep(1);
-        std::cout << "| | |\n" << std::flush;
-        sleep(1);
-        std::cout << "\b\bf" << std::flush;
-        sleep(1);
-        std::cout << "\b-" << std::flush;
-        system("clear");
-    }*/
 }
